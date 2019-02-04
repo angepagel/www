e87ea387b01bdfc1b5b7f3fc7af4bef284f056d8
Ange Pagel <inkandev@gmail.com>
2019-02-04 05:05:25 +0100

    Update footer.js

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,9 +1,42 @@
 import React, { Component } from 'react';
+import { Container, Row, Col } from 'reactstrap';
 
 class Footer extends Component {
   render() {
     return (
       <footer>
+        <Container>
+          <Row>
+            <Col xs="12" sm="6" lg="3">
+              <h5>Ange Pagel</h5>
+              <hr/>
+              <ul>
+                <li>Lorem Ipsum</li>
+              </ul>
+            </Col>
+            <Col xs="12" sm="6" lg="3">
+              <h5>Explorer</h5>
+              <hr/>
+              <ul>
+                <li>Lorem Ipsum</li>
+              </ul>
+            </Col>
+            <Col xs="12" sm="6" lg="3">
+              <h5>Projets</h5>
+              <hr/>
+              <ul>
+                <li>Lorem Ipsum</li>
+              </ul>
+            </Col>
+            <Col xs="12" sm="6" lg="3">
+              <h5>Réseaux</h5>
+              <hr/>
+              <ul>
+                <li>Lorem Ipsum</li>
+              </ul>
+            </Col>
+          </Row>
+        </Container>
       </footer>
     );
   }
