57a27cd94cf0d3897e05c479ddcf4ff255cb1567
Ange Pagel <inkandev@gmail.com>
2019-02-05 01:52:39 +0100

    Create dedicate folder for header

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.scss';
 
-import Header from './components/header';
+import Header from './components/header/header';
 import Footer from './components/footer/footer';
 import Home from './pages/home/home';
 import Resume from './pages/resume/resume';
