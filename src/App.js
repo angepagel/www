fdf42eccaa28baf4dd97eb9c2a06249d7e437c76
Ange Pagel <inkandev@gmail.com>
2019-02-05 01:56:10 +0100

    Import Bootstrap from App.js instead of App.scss

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
 import logo from './logo.svg';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 import './App.scss';
 
 import Header from './components/header/header';
