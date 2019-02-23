ce4b7d184b63e7f6adc80a835633ec0be32e22f0
Ange Pagel <inkandev@gmail.com>
2019-02-23 14:56:56 +0100

    Create Login form

--- src/pages/login/login.js
+++ src/pages/login/login.js
@@ -1,10 +1,37 @@
 import React, { Component } from 'react';
+import {
+  Container,
+  Row,
+  Col,
+  Form,
+  FormGroup,
+  Label,
+  Input,
+  Button
+} from 'reactstrap';
 
 class Login extends Component {
   render() {
     return (
         <div id="login">
           <h1 className="page-title">Connexion</h1>
+            <Container>
+              <Row>
+                <Col className="mx-auto" lg='6'>
+                  <Form>
+                    <FormGroup>
+                      <Label for="username"><i class="material-icons">person</i> Nom d'utilisateur</Label>
+                      <Input type="text" name="username" id="login_username"></Input>
+                    </FormGroup>
+                    <FormGroup>
+                      <Label for="password"><i class="material-icons">security</i> Mot de passe</Label>
+                      <Input type="password" name="password" id="login_password" />
+                    </FormGroup>
+                    <Button>Connexion</Button>
+                  </Form>
+                </Col>
+              </Row>
+            </Container>
         </div>
     );
   }
