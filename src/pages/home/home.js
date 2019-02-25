e98af3bf875d3986888ecd962711834b89787d41
Ange Pagel <inkandev@gmail.com>
2019-02-26 00:34:32 +0100

    Update home.js

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,10 +1,18 @@
 import React, { Component } from 'react';
+import Typed from 'react-typed';
 
 class Home extends Component {
   render() {
     return (
         <div id="home">
-          <h1 className="page-title">Accueil</h1>
+          <h1 className="page-title">
+            <Typed loop
+              strings={['Bienvenue', 'Welcome', 'ようこそ', 'Bienvenidos', 'Willkommen']}
+              cursorChar={'_'}
+              typeSpeed={60}
+              backSpeed={30}
+              backDelay={4000} />
+          </h1>
         </div>
     );
   }
