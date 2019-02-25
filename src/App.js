38a0c4927cd831af74a2bfb3af9aa00ad58a18d4
Ange Pagel <inkandev@gmail.com>
2019-02-25 16:29:35 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
-import logo from './logo.svg';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 import './App.scss';
 
