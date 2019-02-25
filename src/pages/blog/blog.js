481dd1dc3c48718951c62ce4f42454b77e1675b6
Ange Pagel <inkandev@gmail.com>
2019-02-25 17:26:08 +0100

    Update Blog

--- src/pages/blog/blog.js
+++ src/pages/blog/blog.js
@@ -1,10 +1,57 @@
 import React, { Component } from 'react';
+import { Container, Row, Col } from 'reactstrap';
+import './blog.scss';
 
 class Blog extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      posts: [],
+    }
+  }
+
+  componentDidMount() {
+    fetch('/api/posts')
+    .then(res => res.json())
+    .then(json => {
+      this.setState({
+        posts: json.data
+      })
+    })
+    .catch(error => {
+      console.error(error)
+    });
+  }
+
   render() {
+
+    let { posts } = this.state;
+
     return (
         <div id="blog">
           <h1 className="page-title">Blog</h1>
+          <section>
+            <Container>
+              {
+                posts.map(post => (
+                <article key={post.id}>
+                  <Row className="post">
+                    <Col xs='12' md='6' className="post-image" style={{backgroundImage: `url(${post.image})`}} />
+                    <Col xs='12' md='6' className="post-description">
+                      <h2>{post.title}</h2>
+                      <div className="infos">
+                        <span className="category">{post.category}</span>
+                        <span className="publication-date">Publié le {post.date}</span>
+                      </div>
+                      <p>{post.body}</p>
+                      <a className="main" href="#">Lire la suite</a>
+                    </Col>
+                  </Row>
+                </article>
+              ))}
+            </Container>
+          </section>
         </div>
     );
   }
