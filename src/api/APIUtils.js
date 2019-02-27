cc31a7a90a3a72b3b987e8ca0be2dc1e319b7cb0
Ange Pagel <inkandev@gmail.com>
2019-02-28 00:38:36 +0100

    Create login function

--- src/api/APIUtils.js
+++ src/api/APIUtils.js
@@ -32,3 +32,27 @@ export function logout() {
   localStorage.removeItem('token');
 }
 
+export async function login(username, password) {
+  const message = await fetch(`${process.env.REACT_APP_PROXY}/api/login`, {
+    method: 'post',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({
+      username: username,
+      password: password
+    })
+  })
+    .then(
+      res => res.json()
+    )
+    .then(
+      json => {
+        saveToken(json.token);
+        return json.apicode;
+      }
+    )  
+    .catch(
+      error => error
+    );
+
+  return message;
+}
