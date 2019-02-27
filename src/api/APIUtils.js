7ecbe928f088156981a85b22374c7b65c2aaa50b
Ange Pagel <inkandev@gmail.com>
2019-02-27 10:20:34 +0100

    Create getPosts function

---
+++ src/api/APIUtils.js
@@ -0,0 +1,17 @@
+
+export async function getPosts() {
+  let posts = [];
+
+  posts = await fetch(`${process.env.REACT_APP_PROXY}/api/posts`)
+    .then(
+      res => res.json()
+    )
+    .then(
+      json => json.data
+    )
+    .catch(
+      error => console.error(error)
+    );
+
+  return posts;
+}
