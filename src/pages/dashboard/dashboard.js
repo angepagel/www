6e2c504ecf7b858ff7121af42b7dd978bd791a2b
Ange Pagel <inkandev@gmail.com>
2019-03-03 03:44:05 +0100

    Upload Dashboard page

--- src/pages/dashboard/dashboard.js
+++ src/pages/dashboard/dashboard.js
@@ -1,10 +1,24 @@
 import React, { Component } from 'react';
+import { NavLink } from 'react-router-dom';
+import { Container, Row, Col } from 'reactstrap';
+import './dashboard.scss';
 
 class Dashboard extends Component {
   render() {
     return (
         <div id="dashboard">
           <h1 className="page-title">Tableau de bord</h1>
+          <Container>
+            <section>
+              <Row>
+                <Col>
+                  <h2>Gestion du Blog</h2>
+                  <hr/>
+                  <NavLink className="main" to="/dashboard/editor"><i className="material-icons">add</i> Publier</NavLink>
+                </Col>
+              </Row>
+            </section>
+          </Container>
         </div>
     );
   }
