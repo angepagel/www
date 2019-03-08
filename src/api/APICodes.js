890328a9542e08ea4061f13a2020016ca0044a84
Ange Pagel <inkandev@gmail.com>
2019-03-08 02:45:22 +0100

    Update APICodes.js

--- src/api/APICodes.js
+++ src/api/APICodes.js
@@ -1,7 +1,8 @@
 const APICodes = {
   successful_login: "Connexion réussie.",
   bad_credentials: "Identifiants incorrects.",
-  files_uploaded: "Fichiers mis en ligne."
+  files_uploaded: "Fichiers mis en ligne.",
+  post_deleted: "Article de blog supprimé."
 };
   
 export default new Proxy(APICodes, {
