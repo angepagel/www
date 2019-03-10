6ac364f5f0e0d25744b1a3cffb8535ac7f40f744
Ange Pagel <inkandev@gmail.com>
2019-03-10 17:43:54 +0100

    Update resume.js

--- src/pages/resume/resume.js
+++ src/pages/resume/resume.js
@@ -47,7 +47,7 @@ class Resume extends Component {
                       <div className="line">
                         <h3>Cisco CCNA Routing & Switching</h3>
                         <div className="infos">
-                          <span><i className="material-icons">event_note</i> Sept. 2018 - En cours</span>
+                          <span><i className="material-icons">event_note</i> Septembre 2018 - En cours</span>
                           <br/>
                           <span><i className="material-icons">location_on</i> <a rel="noopener noreferrer" target="_blank" href="https://www.google.com/maps/place/E.S.I.R.E.M./@47.3121099,5.0725104,16.75z/data=!4m5!3m4!1s0x47f29e6bd4c577f3:0xb2198f3923ab0998!8m2!3d47.3121732!4d5.0739723">École d'ingénieurs ESIREM</a></span>
                         </div>
@@ -56,7 +56,7 @@ class Resume extends Component {
                       <div className="line">
                         <h3>DUT Informatique</h3>
                         <div className="infos">
-                          <span><i className="material-icons">event_note</i> Sept. 2017 - Juin 2019</span>
+                          <span><i className="material-icons">event_note</i> Septembre 2017 - Juin 2019</span>
                           <br/>
                           <span><i className="material-icons">location_on</i> <a rel="noopener noreferrer" target="_blank" href="https://www.google.com/maps/place/IUT+Dijon/@47.3110124,5.0663128,17z/data=!3m2!4b1!5s0x47f29e6eeec3b081:0xe3076a48a9fe61f1!4m5!3m4!1s0x47f29e6edc000023:0xc034427f4d6a0b09!8m2!3d47.3110124!4d5.0685015">Université de Bourgogne</a></span>
                         </div>
@@ -65,10 +65,13 @@ class Resume extends Component {
                       <div className="line">
                         <h3>Baccalauréat Scientifique</h3>
                         <div className="infos">
-                          <span><i className="material-icons">event_note</i> Sept. 2015 - Juin 2017</span>
+                          <span><i className="material-icons">event_note</i> Septembre 2015 - Juin 2017</span>
                           <br/>
                           <span><i className="material-icons">location_on</i> <a rel="noopener noreferrer" target="_blank" href="https://www.google.com/maps/place/Lyc%C3%A9e+G%C3%A9n%C3%A9ral+Jean-Marc+Boivin/@47.3031215,5.1254075,17z/data=!3m1!4b1!4m5!3m4!1s0x47f29f845f30838f:0x62034e02e5c87c61!8m2!3d47.3031215!4d5.1275962">Lycée Général Jean-Marc Boivin</a></span>
                         </div>
+                        <p className="description">
+                          Diplôme obtenu avec <strong>mention Très Bien</strong>, section européenne espagnol.
+                        </p>
                       </div>
                     </Col>
                   </Row>
@@ -83,12 +86,27 @@ class Resume extends Component {
                     </Col>
                     <Col xs="12" sm="7">
                       <div className="line">
+                        <h3>Stage en Centre de Recherche</h3>
+                        <div className="infos">
+                          <span><i className="material-icons">event_note</i> Avril - Juillet 2019</span>
+                          <br/>
+                          <span><i className="material-icons">location_on</i> <a rel="noopener noreferrer" target="_blank" href="https://www.google.fr/maps/place/BU-CROCCS/@14.0401609,100.6081118,17z/data=!4m5!3m4!1s0x30e2801accd02c07:0xdb5174318f1046b0!8m2!3d14.0401174!4d100.6102877">Bangkok University CROCCS</a></span>
+                        </div>
+                        <p className="description">
+                          Projet « <strong>A2T</strong> ».
+                        </p>
+                      </div>
+
+                      <div className="line">
                         <h3>Hackathon WebStudentCup</h3>
                         <div className="infos">
                           <span><i className="material-icons">event_note</i> 1 & 2 Février 2019</span>
                           <br/>
                           <span><i className="material-icons">location_on</i> <a rel="noopener noreferrer" target="_blank" href="https://www.google.com/maps/place/E.S.I.R.E.M./@47.3121099,5.0725104,16.75z/data=!4m5!3m4!1s0x47f29e6bd4c577f3:0xb2198f3923ab0998!8m2!3d47.3121732!4d5.0739723">École d'ingénieurs ESIREM</a></span>
                         </div>
+                        <p className="description">
+                          Équipe classée <strong>3e sur 11</strong>.
+                        </p>
                       </div>
 
                       <div className="line">
@@ -98,6 +116,9 @@ class Resume extends Component {
                           <br/>
                           <span><i className="material-icons">location_on</i> <a rel="noopener noreferrer" target="_blank" href="https://www.google.fr/maps/place/Campus+Universitaire+d'Auxerre+(IUT,+UFR,+Le2i)/@47.7892,3.5920305,18z/data=!4m8!1m2!2m1!1sIUT+Auxerre!3m4!1s0x0:0x54e46eb902a3559!8m2!3d47.7886641!4d3.592537">IUT d'Auxerre</a></span>
                         </div>
+                        <p className="description">
+                          Équipe classée <strong>8e sur 9</strong>.
+                        </p>
                       </div>
 
                       <div className="line">
@@ -116,6 +137,9 @@ class Resume extends Component {
                           <br/>
                           <span><i className="material-icons">location_on</i> <a rel="noopener noreferrer" target="_blank" href="https://www.google.fr/maps/place/IUT+Dijon/@47.3110124,5.0663128,17z/data=!3m2!4b1!5s0x47f29e6eeec3b081:0xe3076a48a9fe61f1!4m5!3m4!1s0x47f29e6edc000023:0xc034427f4d6a0b09!8m2!3d47.3110124!4d5.0685015">IUT de Dijon</a></span>
                         </div>
+                        <p className="description">
+                          Convention d'anime, participation bénévole à l'événement.
+                        </p>
                       </div>
                     </Col>
                   </Row>
