a1f23fd6ff1ea2ebf01537d6a0fb3c1a36984b33
Ange Pagel <inkandev@gmail.com>
2019-03-10 23:44:48 +0100

    Update APICodes.js

--- src/api/APICodes.js
+++ src/api/APICodes.js
@@ -2,6 +2,8 @@ const APICodes = {
   successful_login: "Connexion réussie.",
   bad_credentials: "Identifiants incorrects.",
   files_uploaded: "Fichiers mis en ligne.",
+  post_created: "Article de blog créé.",
+  post_updated: "Article de blog mis à jour.",
   post_deleted: "Article de blog supprimé."
 };
   
