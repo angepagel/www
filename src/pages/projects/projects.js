a08f0fccd28c55e14f05940e6a1410a7efa89b41
Ange Pagel <inkandev@gmail.com>
2019-03-27 19:13:14 +0100

    Update project: Civilization Zero

--- src/pages/projects/projects.js
+++ src/pages/projects/projects.js
@@ -69,7 +69,45 @@ class Projects extends Component {
                     </Row>
                   </article>
                 </Col>
-              </Row>  
+              </Row>
+              <Row>
+                <Col xs='12' md='6'>
+                  <article id="project-durandal">
+                    <Row className="project">
+                      <Col xl='12' className="project-image" style={{backgroundImage: `url(${require('../../media/civilization-zero.png')})`}} />
+                      <Col xl='12' className="project-description">
+                        <h2>Civilization Zero</h2>
+                        <div className="infos">
+                          <span className="category">Jeu vidéo</span>
+                          <span className="date">Nov. - Déc. 2017</span>
+                        </div>
+                        <p>
+                          Civilization Zero est un jeu en ligne de commande développé en Pascal et inspiré de la célèbre série de jeux vidéo Civilization. Le joueur construit et améliore des bâtiments pour produire diverses ressources et se développer. Il doit également se protéger et diriger son armée contre les ennemis qui l'assaillissent.
+                        </p>
+                        <a className="main" rel="noopener noreferrer" href="https://github.com/angepagel/Civilization-Zero-Lamaths-UE" target="_blank">Voir sur GitHub</a>
+                      </Col>
+                    </Row>
+                  </article>
+                </Col>
+                <Col xs='12' md='6'>
+                  <article>
+                    <Row className="project">
+                      <Col xl='12' className="project-image" />
+                      <Col xl='12' className="project-description">
+                        <h2>Projet 5</h2>
+                        <div className="infos">
+                          <span className="category">Catégorie</span>
+                          <span className="date">Date</span>
+                        </div>
+                        <p>
+                          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut ultrices venenatis eros quis porta. Maecenas eget elit nec velit egestas rhoncus. Suspendisse sem lacus, convallis eget malesuada at, viverra eget nisi. Donec finibus ac erat eu molestie.
+                        </p>
+                        <a className="main" href="/">Voir sur GitHub</a>
+                      </Col>
+                    </Row>
+                  </article>
+                </Col>
+              </Row>
             </Container>
           </section>
         </div>
