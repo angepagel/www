86c341e0403db2d3d342a9c0c3e4abcd4bef9a37
Ange Pagel <inkandev@gmail.com>
2019-04-13 00:45:16 +0200

    Add GitHub icons to GitHub links

--- src/pages/projects/projects.js
+++ src/pages/projects/projects.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Helmet } from 'react-helmet';
 import { Container, Row, Col } from 'reactstrap';
+import { GitHubIcon } from '../../components/svg/icons';
 import './projects.scss';
 
 class Projects extends Component {
@@ -27,8 +28,8 @@ class Projects extends Component {
                       <p>
                         Mon site internet personnel, conçu à l'aide de Node.js et des frameworks React (front-end) et Express (back-end, REST API). Son développement s'appuie sur une multitude de technologies web (Bootstrap, JSON Web Tokens, Sass, React Router, etc.) qui me permettent de présenter mon profil professionnel de manière soignée et originale tout en mettant à ma disposition un support d'expression personnel.
                       </p>
-                      <a className="main mr-4" rel="noopener noreferrer" href="https://github.com/angepagel/www-front" target="_blank">Front-end (GitHub)</a>
-                      <a className="main mt-2" rel="noopener noreferrer" href="https://github.com/angepagel/www-back" target="_blank">Back-end (GitHub)</a>
+                      <a className="main mr-4" rel="noopener noreferrer" href="https://github.com/angepagel/www-front" target="_blank">Front-end (GitHub <GitHubIcon/>)</a>
+                      <a className="main mt-2" rel="noopener noreferrer" href="https://github.com/angepagel/www-back" target="_blank">Back-end (GitHub <GitHubIcon/>)</a>
                     </Col>
                 </Row>
               </article>
@@ -46,7 +47,7 @@ class Projects extends Component {
                         <p>
                           Durandal est un bot Discord développé en Python. Il est capable d'effectuer des actions basiques et de répondre à diverses commandes.
                         </p>
-                        <a className="main" rel="noopener noreferrer" href="https://github.com/angepagel/Durandal" target="_blank">Voir sur GitHub</a>
+                        <a className="main" rel="noopener noreferrer" href="https://github.com/angepagel/Durandal" target="_blank">Voir sur GitHub <GitHubIcon/></a>
                       </Col>
                     </Row>
                   </article>
@@ -64,7 +65,6 @@ class Projects extends Component {
                         <p>
                           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut ultrices venenatis eros quis porta. Maecenas eget elit nec velit egestas rhoncus. Suspendisse sem lacus, convallis eget malesuada at, viverra eget nisi. Donec finibus ac erat eu molestie.
                         </p>
-                        <a className="main" href="/">Voir sur GitHub</a>
                       </Col>
                     </Row>
                   </article>
@@ -84,7 +84,7 @@ class Projects extends Component {
                         <p>
                           Civilization Zero est un jeu en ligne de commande développé en Pascal et inspiré de la célèbre série de jeux vidéo Civilization. Le joueur construit et améliore des bâtiments pour produire diverses ressources et se développer. Il doit également se protéger et diriger son armée contre les ennemis qui l'assaillissent.
                         </p>
-                        <a className="main" rel="noopener noreferrer" href="https://github.com/angepagel/Civilization-Zero-Lamaths-UE" target="_blank">Voir sur GitHub</a>
+                        <a className="main" rel="noopener noreferrer" href="https://github.com/angepagel/Civilization-Zero-Lamaths-UE" target="_blank">Voir sur GitHub <GitHubIcon/></a>
                       </Col>
                     </Row>
                   </article>
