adecfee1cafd229467b7c7f0f5424d1e71ad57d8
Ange Pagel <inkandev@gmail.com>
2019-04-22 09:45:15 +0200

    Add FR & EN support for Footer component (i18n)

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { NavLink } from 'react-router-dom';
 import { Container, Row, Col } from 'reactstrap';
+import { FormattedMessage } from 'react-intl';
 import './footer.scss';
 import * as API from '../../api/APIUtils';
 
@@ -9,7 +10,7 @@ class Footer extends Component {
 
     let signedin = (
       <div id="signedin">
-        Vous êtes connecté. <NavLink className="main ml-1" onClick={API.logout} to="/login">Déconnexion</NavLink>
+        <FormattedMessage id="Footer.signedin.info" /> <NavLink className="main ml-1" onClick={API.logout} to="/login"><FormattedMessage id="Footer.signedin.logout" /></NavLink>
       </div>
     );
 
@@ -21,22 +22,22 @@ class Footer extends Component {
               <h5>Ange Pagel</h5>
               <hr/>
               <ul>
-                <li><NavLink to="/about">À propos</NavLink></li>
+                <li><NavLink to="/about"><FormattedMessage id="Footer.about.link" /></NavLink></li>
                 <li><a href="mailto:angepagel@gmail.com">angepagel@gmail.com</a></li>
                 <li><a href="tel:+33651705341">06 51 70 53 41</a></li>
               </ul>
             </Col>
             <Col xs='12' sm='6' lg='3'>
-              <h5>Explorer</h5>
+              <h5><FormattedMessage id="Footer.explore.header" /></h5>
               <hr/>
               <ul>
-                <li><NavLink to="/resume">Curriculum Vitæ</NavLink></li>
-                <li><NavLink to="/projects">Projets</NavLink></li>
-                <li><NavLink to="/blog">Blog</NavLink></li>
+                <li><NavLink to="/resume"><FormattedMessage id="Footer.explore.resume" /></NavLink></li>
+                <li><NavLink to="/projects"><FormattedMessage id="Footer.explore.projects" /></NavLink></li>
+                <li><NavLink to="/blog"><FormattedMessage id="Footer.explore.blog" /></NavLink></li>
               </ul>
             </Col>
             <Col xs='12' sm='6' lg='3'>
-              <h5>Projets</h5>
+              <h5><FormattedMessage id="Footer.projects.header" /></h5>
               <hr/>
               <ul>
                 <li><a rel="noopener noreferrer" href="https://github.com/angepagel/ThreeJS-Lab" target="_blank">ThreeJS Lab</a></li>
@@ -45,7 +46,7 @@ class Footer extends Component {
               </ul>
             </Col>
             <Col xs='12' sm='6' lg='3'>
-              <h5>Réseaux</h5>
+              <h5><FormattedMessage id="Footer.networks.header" /></h5>
               <hr/>
               <ul>
                 <li><a rel="noopener noreferrer" href="https://github.com/angepagel" target="_blank">GitHub</a></li>
@@ -55,7 +56,7 @@ class Footer extends Component {
           </Row>
         </Container>
         {API.isAuth() ? signedin : ''}
-        <div id='mentions'>Développé avec <span role="img" aria-label="heart">❤️</span> par Ange Pagel</div>
+        <div id='mentions'><FormattedMessage id="Footer.mentions" /></div>
       </footer>
     );
   }
