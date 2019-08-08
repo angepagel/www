b887d6d60cf73ca66524a19c3f589482b646929c
Ange Pagel <inkandev@gmail.com>
2019-08-08 08:16:55 +0200

    Create Preview component
    Component used to preview blog posts.

---
+++ src/components/blog/preview/preview.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import { NavLink } from 'react-router-dom';
+import { Row, Col } from 'reactstrap';
+import { FormattedMessage } from 'react-intl';
+import './preview.scss';
+
+class Preview extends Component {
+
+    render() {
+        return (
+            <Row className="preview">
+                <Col sm='12' md='6' className="image" style={{backgroundImage: this.props.imageUrl}} />
+                <Col sm='12' md='6' className="description">
+
+                    <h2>{this.props.title}</h2>
+                    
+                    <div className="infos">
+                        <span className="category">{this.props.category}</span>
+                        <span className="date">
+                            <FormattedMessage id="Preview.published_on"/> {this.props.date}
+                        </span>
+                    </div>
+
+                    <div dangerouslySetInnerHTML={{__html:this.props.content}} />
+
+                    <NavLink className="main" to={this.props.linkTo}>
+                        <FormattedMessage id="Preview.read_more"/>
+                    </NavLink>
+                    
+                </Col>
+            </Row>
+        );
+    }
+
+}
+
+export default Preview;
