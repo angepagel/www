92037c005d81c9e95b485b86ca9a728c5d2f062b
Ange Pagel <angepagel@gmail.com>
2019-08-27 22:16:57 +0200

    Update 404 Page

--- src/pages/notFound/notFound.js
+++ src/pages/notFound/notFound.js
@@ -1,10 +1,32 @@
 import React, { Component } from 'react';
+import { Helmet } from 'react-helmet';
+import { NavLink } from 'react-router-dom';
+import { Container, Row, Col } from 'reactstrap';
+import { FormattedMessage } from 'react-intl';
+import { PageNotFound } from '../../components/svg/unDraw';
+import './notFound.scss';
 
 class NotFound extends Component {
   render() {
     return (
         <div id="notFound">
-          <h1 className="page-title">404</h1>
+
+          <Helmet>
+            <title>Ange Pagel – 404</title>
+          </Helmet>
+
+          <Container>
+            <Row>
+              <Col>
+                <h1 className="page-title"><FormattedMessage id="NotFound.title"/></h1>
+                <PageNotFound/>
+                <p>
+                  <FormattedMessage id="NotFound.message"/> <NavLink exact to="/"><FormattedMessage id="NotFound.redirection"/></NavLink>
+                </p>
+              </Col>
+            </Row>
+          </Container>
+
         </div>
     );
   }
